603b10c46001561b25fcb43524ebf434d9bed542
doutenn <95846055+doutenn@users.noreply.github.com>
2022-06-16 16:58:28 +0900

    Update b-c.js

--- b-c.js
+++ b-c.js
@@ -26,7 +26,27 @@ const main = () => {
         resetButton.onclick = function() {
           reset();
         };
-      };
+        //描画開始
+        ctx.beginPath();
+        //円
+        ctx.arc(150, 400, 7, 0, Math.PI*2, false)
+        ctx.fill();
+ 
+        //三角形
+        ctx.moveTo(x, 400);
+        ctx.lineTo(650, 100);
+        ctx.lineTo(650, 400);
+
+        ctx.font = "32px serif";
+        ctx.fillText("B",120,410);
+        ctx.fillText("A",660,110);
+        ctx.fillText("C",660,410);
+
+        // 開始地点に戻って線を閉じる
+        ctx.closePath();
+        // 塗りつぶしを実行
+        ctx.stroke();
+    };
    
     function draw(){
       if(x <= 650){
@@ -47,7 +67,7 @@ const main = () => {
        // y を 1 増加
        x += 1;
        // 10 ミリ秒(0.1 秒)後に draw を再度呼び出し
-       setTimeout(draw, 10, x - 1);
+       setTimeout(draw, 10, x + 1);
 
   
        //三角形
